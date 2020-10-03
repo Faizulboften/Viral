577e49109370c289b9f9fa53e18b86a6ce5b7447
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-03 13:02:25 +0800

    Create croot.sh

---
+++ croot.sh
@@ -0,0 +1,27 @@
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+clear
+sleep 1
+echo $me "
+█████████
+\033[1;93m█▄█████▄█      \033[1;91m●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+\033[1;93m█\033[1;92m▼▼▼▼▼ \033[1;92m- _ --_--\033[1;95mBOKEP 
+\033[1;93m█ \033[1;92m \033[1;92m_-_-- -_ --__\033[1;93mBOKEP
+\033[1;93m█\033[1;92m▲▲▲▲▲\033[1;92m--  - _ --\033[1;96mBOKEP\033[1;96m tools-BOKEP
+\033[1;93m█████████      \033[1;92m«----------✧----------»
+\033[1;93m ██ ██"
+
+sleep 10
+echo $cyan "⭕ MENGINSTALL BOKEP "
+sleep 10
+echo $cyan "⭕ OPEN VPN "
+sleep 10
+echo $cyan "⭕ OPEN WEBSITE"
+sleep 10
+termux-open https://inibokep.pro/2020/08/30/bokep-indo-2-bocah-sd-di-ajarin-mesum-di-semak-semak/
